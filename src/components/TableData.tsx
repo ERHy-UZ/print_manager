fb8f9d295a45075951917bb677a5fa2e4f516b2c
ERHy-UZ <jtrejom210900@gmail.com>
2024-11-13 10:17:32 -0600

    Mejoras a la forma en la que se muestra la tabla

--- src/components/TableData.tsx
+++ src/components/TableData.tsx
@@ -23,25 +23,21 @@ export default function TableData() {
 
     return (
         <main className='border bg-egg-500 border-black mt-5 text-xs overflow-x-auto'>
-            { state.search_text.size > 5 ?
+            {records.length ?
                 <table>
                     <thead>
-                        {records.map((record, index) => (
-                            <tr key={index} className='border-b border-black'>
-                                {index === 0 &&
-                                    record.map((item, index) => (
-                                        <th key={index} className='border-r border-black'>{item}</th>
-                                    ))}
-                            </tr>
-                        ))}
+                        <tr className='border-b border-black'>
+                            {records[0].map((record, index) => (
+                                <th key={index} className='border-r border-black last-of-type:border-r-0'>{record}</th>
+                            ))}
+                        </tr>
                     </thead>
                     <tbody>
-                        {records.map((record, index) => (
-                            <tr key={index} className='border-b border-black'>
-                                {index !== 0 &&
-                                    record.map((item, index) => (
-                                        <td key={index} className='border-r border-black'>{item}</td>
-                                    ))}
+                        {records.filter((records, index) => index > 0).map((record, index) => (
+                            <tr key={index} className='border-b last-of-type:border-b-0 border-black'>
+                                {record.map((item, index) => (
+                                    <td key={index} className='border-r border-black last-of-type:border-r-0'>{item}</td>
+                                ))}
                             </tr>
                         ))}
                     </tbody>
