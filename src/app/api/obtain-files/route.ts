3885db497888f15e1fe87feded2710f60bae8a79
ERHy-UZ <jtrejom210900@gmail.com>
2024-11-14 17:15:45 -0600

    Api que trae los archivos de drive

--- src/app/api/obtain-files/route.ts
+++ src/app/api/obtain-files/route.ts
@@ -1,8 +1,19 @@
 import { NextRequest, NextResponse } from "next/server"
+import { google } from "googleapis"
 
 export async function GET() {
-    
+    const auth = new google.auth.GoogleAuth({
+        keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+        scopes: ['https://www.googleapis.com/auth/drive']
+    })
+
+    const drive = google.drive({ version: 'v3', auth })
+    const files = await drive.files.list({
+        q: 'trashed: False'
+    })
+
     return NextResponse.json({
-        test: 'test'
+        files: files.data.files?.filter(file => file.mimeType === 'application/vnd.google-apps.folder')
     })
 }
+
