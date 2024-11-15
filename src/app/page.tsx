5d38fe0a34ccb8a8f001913cdf50ad9ec893d562
ERHy-UZ <jtrejom210900@gmail.com>
2024-11-15 17:22:45 -0600

    Se añadio la variable de entorno para usarla en la vista de archivos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import FileList from "@/components/FileList";
 import SearchBar from "@/components/SearchBar";
 import TableData from "@/components/TableData";
 
 export default function Home() {
 
+  const main_file = process.env.MAIN_FILE
+
   return (
     <div className='mx-48 my-10'>
       <SearchBar />
       <TableData />
+      <FileList main_file={main_file!}/>
     </div>
   );
 }
