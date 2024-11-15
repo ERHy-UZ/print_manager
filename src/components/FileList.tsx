8fafd878a1c30b13817ceaf8c4c4b3ccdab061c5
ERHy-UZ <jtrejom210900@gmail.com>
2024-11-15 17:37:37 -0600

    se movieron las variable/constantes al reducer

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { GDriveFileType } from "@/types"
+import { useManager } from "@/app/hooks/useManager";
 import { useState, useEffect } from "react"
 import { FaFolder, FaFile, FaFileCircleXmark } from "react-icons/fa6";
 import { PiKeyReturnFill } from "react-icons/pi";
@@ -11,36 +11,35 @@ type FileListProps = {
 
 export default function FileList({ main_file }: FileListProps) {
 
-    const [gFiles, setGFiles] = useState<GDriveFileType[]>([])
+    const { state, dispatch } = useManager()
     const [history, setHistory] = useState<string[]>([main_file])
-    const [selectedFile, setSelectedFile] = useState<string>('')
     const [isBackSelected, setBackSelected] = useState<boolean>(false)
 
     useEffect(() => {
         fetch(`/api/obtain-files`)
             .then(response => response.json())
-            .then(data => setGFiles(data.files))
+            .then(data => dispatch({ type: 'set-files', payload: { files: data.files } }))
             .catch(error => console.log('Error: ', error))
     }, [])
 
     useEffect(() => {
-        if (selectedFile) {
-            if (!isBackSelected) { setHistory([...history, selectedFile]) }
+        if (state.selectedFile) {
+            if (!isBackSelected) { setHistory([...history, state.selectedFile]) }
 
             setBackSelected(false)
 
-            fetch(`/api/obtain-files/${selectedFile}`)
+            fetch(`/api/obtain-files/${state.selectedFile}`)
                 .then(response => response.json())
-                .then(data => setGFiles(data.files))
+                .then(data => dispatch({ type: 'set-files', payload: { files: data.files } }))
                 .catch(error => console.log('Error: ', error))
         }
-    }, [selectedFile])
+    }, [state.selectedFile])
 
     const handleBackButton = () => {
         if (history.length > 1) {
             setBackSelected(true)
 
-            setSelectedFile(history[history.length - 2])
+            dispatch({ type: 'set-selected-file', payload: { file: history[history.length - 2] } })
             setHistory(history.filter((file, index) => index != history.length - 1))
         }
     }
@@ -50,12 +49,12 @@ export default function FileList({ main_file }: FileListProps) {
             <menu className='bg-orange-300 border-b border-orange-500 mb-5 p-1'>
                 <PiKeyReturnFill className={`text-2xl ${history.length == 1 && 'text-orange-700'} ${history.length > 1 && 'cursor-pointer'}`} onClick={handleBackButton} />
             </menu>
-            {gFiles.length > 0 && gFiles[0].mimeType !== 'undef' ?
+            {state.gFiles.length > 0 && state.gFiles[0].mimeType !== 'undef' ?
                 <ol className='grid grid-cols-4 gap-3'>
-                    {gFiles.map((file, index) => (
+                    {state.gFiles.map((file, index) => (
                         <li key={index} className='flex flex-col justify-center items-center'>
                             {file.mimeType === 'application/vnd.google-apps.folder' ?
-                                <FaFolder className='text-[5rem] text-amber-500 hover:text-amber-400 cursor-pointer' onDoubleClick={() => setSelectedFile(file.id)} />
+                                <FaFolder className='text-[5rem] text-amber-500 hover:text-amber-400 cursor-pointer' onDoubleClick={() => dispatch({ type: 'set-selected-file', payload: { file: file.id } })} />
                                 :
                                 file.name.endsWith('csv') ?
                                     <FaFile className='text-[5rem] text-blue-400 hover:text-blue-300 cursor-pointer' />
@@ -67,7 +66,7 @@ export default function FileList({ main_file }: FileListProps) {
                     ))}
                 </ol>
                 :
-                gFiles.length > 0 ?
+                state.gFiles.length > 0 ?
                     <p className='uppercase text-2xl font-medium bg-red-300 border border-red-500 rounded-sm py-3 px-3 mx-14 text-center'>Carpeta Vacia</p>
                     :
                     <p className='uppercase text-2xl font-medium bg-orange-300 border border-orange-500 rounded-sm py-3 px-3 mx-14 text-center'>Cargando...</p>
--- src/reducers/manager-reducer.ts
+++ src/reducers/manager-reducer.ts
@@ -1,13 +1,20 @@
+import { GDriveFileType } from "@/types"
 
 export type ManagerActions =
-    { type: 'set-search-text', payload: {search: File | null} }
+    { type: 'set-search-text', payload: { search: File | null } } |
+    { type: 'set-files', payload: { files: GDriveFileType[] } } |
+    { type: 'set-selected-file', payload: { file: string } }
 
 export type ManagerState = {
     search_text: File | null
+    gFiles: GDriveFileType[]
+    selectedFile: string
 }
 
 export const initialState: ManagerState = {
-    search_text: typeof window !== "undefined" ? new File(['empty'], 'Seleccione CSV...') : null
+    search_text: typeof window !== "undefined" ? new File(['empty'], 'Seleccione CSV...') : null,
+    gFiles: [],
+    selectedFile: ''
 }
 
 export const managerReducer = (
@@ -20,6 +27,22 @@ export const managerReducer = (
         return {
             ...state,
             search_text: action.payload.search
+        }
+    }
+
+    if (action.type === 'set-files') {
+
+        return {
+            ...state,
+            gFiles: action.payload.files
+        }
+    }
+
+    if (action.type === 'set-selected-file') {
+
+        return {
+            ...state,
+            selectedFile: action.payload.file
         }
     }
 
