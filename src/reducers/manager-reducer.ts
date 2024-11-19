d08d44de48206ee6ceb47fa4a23c5bb846727111
ERHy-UZ <jtrejom210900@gmail.com>
2024-11-19 11:38:27 -0600

    Se agregaron las acciones para añadir csv y se movio el state de records a reducer

--- src/reducers/manager-reducer.ts
+++ src/reducers/manager-reducer.ts
@@ -3,18 +3,25 @@ import { GDriveFileType } from "@/types"
 export type ManagerActions =
     { type: 'set-search-text', payload: { search: File | null } } |
     { type: 'set-files', payload: { files: GDriveFileType[] } } |
-    { type: 'set-selected-file', payload: { file: string } }
+    { type: 'set-selected-file', payload: { file: string } } |
+    { type: 'set-selected-csv', payload: { csv: string } } |
+    { type: 'set-records', payload: {records: string[][]}}
+    
 
 export type ManagerState = {
     search_text: File | null
     gFiles: GDriveFileType[]
     selectedFile: string
+    selectedCSV: string
+    records: string[][]
 }
 
 export const initialState: ManagerState = {
     search_text: typeof window !== "undefined" ? new File(['empty'], 'Seleccione CSV...') : null,
     gFiles: [],
-    selectedFile: ''
+    selectedFile: '',
+    selectedCSV: '',
+    records: []
 }
 
 export const managerReducer = (
@@ -43,6 +50,22 @@ export const managerReducer = (
         return {
             ...state,
             selectedFile: action.payload.file
+        }
+    }
+
+    if (action.type === 'set-selected-csv') {
+
+        return {
+            ...state,
+            selectedCSV: action.payload.csv
+        }
+    }
+
+    if (action.type === 'set-records') {
+
+        return {
+            ...state,
+            records: action.payload.records
         }
     }
 
