76484cf29096117266ee913e19544df4a99ea9c1
ERHy-UZ <jtrejom210900@gmail.com>
2024-11-21 14:18:46 -0600

    Se agrego el header general con el nombre de la aplicación web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,15 @@ export default function Home() {
   const main_file = process.env.MAIN_FILE
 
   return (
-    <div className='mx-48 my-10'>
-      <FileWindow main_file={main_file!}/>
-      <TableData />
-    </div>
+    <>
+      <header className='bg-pantone-7420 px-5 py-3'>
+        <h1 className='uppercase text-white text-2xl font-bold'>Print Log <span className='bg-white text-pantone-7420 px-2 py-[2px]'>IFREM</span></h1>
+      </header>
+      <div className='mx-48 my-10'>
+        <FileWindow main_file={main_file!} />
+        <TableData />
+      </div >
+    </>
+
   );
 }
